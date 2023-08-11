df93fd8807bd9c3f892e24f7a60be722ceea7de5
Brobss <gabrieldomingos13@gmail.com>
2023-08-11 14:49:48 -0300

    ItensCompra model

--- livraria/admin.py
+++ livraria/admin.py
@@ -4,10 +4,11 @@ from .models import Categoria
 from .models import Editora
 from .models import Autor
 from .models import Livro
-from .models import Compra
+from .models import Compra, ItensCompra
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Autor)
 admin.site.register(Livro)
 admin.site.register(Compra)
+admin.site.register(ItensCompra)
--- livraria/models/compra.py
+++ livraria/models/compra.py
@@ -28,7 +28,15 @@ class Compra(models.Model):
     data = models.DateField(auto_now_add=True)
     quantidade = models.IntegerField(default=1)
     valor = models.DecimalField(max_digits=6, decimal_places=2)
-    status = models.CharField(max_length=1, choices=StatusCompra.choices, default=StatusCompra.CARRINHO)
+    status = models.IntegerField(
+        choices=StatusCompra.choices, default=StatusCompra.CARRINHO
+    )
 
     def __str__(self):
         return self.livro.titulo
+
+
+class ItensCompra(models.Model):
+    compra = models.ForeignKey(Compra, on_delete=models.CASCADE, related_name="itens")
+    livro = models.ForeignKey(Livro, on_delete=models.PROTECT, related_name="+")
+    quantidade = models.IntegerField(default=1)
