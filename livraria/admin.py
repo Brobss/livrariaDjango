6a8609edaca2ea0eb22b93814c0bbcfbe914dca8
Brobss <gabrieldomingos13@gmail.com>
2023-08-11 14:53:31 -0300

    ItensCompra TabularInLine

--- livraria/admin.py
+++ livraria/admin.py
@@ -10,5 +10,14 @@ admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Autor)
 admin.site.register(Livro)
-admin.site.register(Compra)
-admin.site.register(ItensCompra)
+# admin.site.register(Compra)
+# admin.site.register(ItensCompra)
+
+
+class ItensCompraInline(admin.TabularInline):
+    model = ItensCompra
+
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    inlines = [ItensCompraInline]
